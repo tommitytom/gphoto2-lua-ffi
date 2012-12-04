bc30fb2f2010b9b70100b632bd1fd72122400676
tommitytom <tommitytom@gmail.com>
2012-12-04 00:57:06 +0000

    Some tidy ups

--- capture-test.lua
+++ capture-test.lua
@@ -1,10 +1,8 @@
-package.path = "./?/init.lua;" .. package.path
-package.loaded [ "gphoto2" ] = dofile ( "init.lua" )
 local gphoto2 = require "gphoto2"
 
 print "Acquiring camera..."
-local camera = gphoto2.acquire_camera()
+local camera = gphoto2.acquireCamera()
 print "Displaying summary..."
 print(camera:summary())
 print "Capturing image..."
-file = camera:capture_image()
+file = camera:captureImage()
--- gphoto.lua
+++ gphoto.lua
@@ -1,9 +1,7 @@
 -- luajit ffi based bindings for gphoto2
 -- http://www.gphoto.org/
 
-local ffi = require("ffi")
-
-assert(jit, "jit table unavailable")
+local ffi = require"ffi"
 
 ffi.cdef[[
 
@@ -24,38 +22,38 @@ ffi.cdef[[
 
 	typedef enum 
 	{
-		GP_DEVICE_STILL_CAMERA          = 0,	 /**< Traditional still camera */
-		GP_DEVICE_AUDIO_PLAYER          = 1 << 0 /**< Audio player */
+			GP_DEVICE_STILL_CAMERA          = 0,	 /**< Traditional still camera */
+			GP_DEVICE_AUDIO_PLAYER          = 1 << 0 /**< Audio player */
 	} GphotoDeviceType;
 
 	typedef enum 
 	{
-		GP_OPERATION_NONE       	= 0,	  /**< No remote control operation supported. */
-		GP_OPERATION_CAPTURE_IMAGE      = 1 << 0, /**< Capturing images supported. */
-		GP_OPERATION_CAPTURE_VIDEO      = 1 << 1, /**< Capturing videos supported. */
-		GP_OPERATION_CAPTURE_AUDIO      = 1 << 2, /**< Capturing audio supported. */
-		GP_OPERATION_CAPTURE_PREVIEW    = 1 << 3, /**< Capturing image previews supported. */
-		GP_OPERATION_CONFIG             = 1 << 4, /**< Camera and Driver configuration supported. */
-		GP_OPERATION_TRIGGER_CAPTURE    = 1 << 5  /**< Camera can trigger capture and wait for events. */
+			GP_OPERATION_NONE       	= 0,	  /**< No remote control operation supported. */
+			GP_OPERATION_CAPTURE_IMAGE      = 1 << 0, /**< Capturing images supported. */
+			GP_OPERATION_CAPTURE_VIDEO      = 1 << 1, /**< Capturing videos supported. */
+			GP_OPERATION_CAPTURE_AUDIO      = 1 << 2, /**< Capturing audio supported. */
+			GP_OPERATION_CAPTURE_PREVIEW    = 1 << 3, /**< Capturing image previews supported. */
+			GP_OPERATION_CONFIG             = 1 << 4, /**< Camera and Driver configuration supported. */
+			GP_OPERATION_TRIGGER_CAPTURE    = 1 << 5  /**< Camera can trigger capture and wait for events. */
 	} CameraOperation;
 
 	typedef enum 
 	{
-		GP_FILE_OPERATION_NONE          = 0,      /**< No special file operations, just download. */
-		GP_FILE_OPERATION_DELETE        = 1 << 1, /**< Deletion of files is possible. */
-		GP_FILE_OPERATION_PREVIEW       = 1 << 3, /**< Previewing viewfinder content is possible. */
-		GP_FILE_OPERATION_RAW           = 1 << 4, /**< Raw retrieval is possible (used by non-JPEG cameras) */
-		GP_FILE_OPERATION_AUDIO         = 1 << 5, /**< Audio retrieval is possible. */
-		GP_FILE_OPERATION_EXIF          = 1 << 6  /**< EXIF retrieval is possible. */
+			GP_FILE_OPERATION_NONE          = 0,      /**< No special file operations, just download. */
+			GP_FILE_OPERATION_DELETE        = 1 << 1, /**< Deletion of files is possible. */
+			GP_FILE_OPERATION_PREVIEW       = 1 << 3, /**< Previewing viewfinder content is possible. */
+			GP_FILE_OPERATION_RAW           = 1 << 4, /**< Raw retrieval is possible (used by non-JPEG cameras) */
+			GP_FILE_OPERATION_AUDIO         = 1 << 5, /**< Audio retrieval is possible. */
+			GP_FILE_OPERATION_EXIF          = 1 << 6  /**< EXIF retrieval is possible. */
 	} CameraFileOperation;
 
 	typedef enum 
 	{
-		GP_FOLDER_OPERATION_NONE        = 0,	  /**< No special filesystem operation. */
-		GP_FOLDER_OPERATION_DELETE_ALL  = 1 << 0, /**< Deletion of all files on the device. */
-		GP_FOLDER_OPERATION_PUT_FILE    = 1 << 1, /**< Upload of files to the device possible. */
-		GP_FOLDER_OPERATION_MAKE_DIR    = 1 << 2, /**< Making directories on the device possible. */
-		GP_FOLDER_OPERATION_REMOVE_DIR  = 1 << 3  /**< Removing directories from the device possible. */
+			GP_FOLDER_OPERATION_NONE        = 0,	  /**< No special filesystem operation. */
+			GP_FOLDER_OPERATION_DELETE_ALL  = 1 << 0, /**< Deletion of all files on the device. */
+			GP_FOLDER_OPERATION_PUT_FILE    = 1 << 1, /**< Upload of files to the device possible. */
+			GP_FOLDER_OPERATION_MAKE_DIR    = 1 << 2, /**< Making directories on the device possible. */
+			GP_FOLDER_OPERATION_REMOVE_DIR  = 1 << 3  /**< Removing directories from the device possible. */
 	} CameraFolderOperation;
 
 	typedef enum
@@ -81,15 +79,6 @@ ffi.cdef[[
 		GP_WIDGET_BUTTON,	/**< \brief Button press widget. */		/* CameraWidgetCallback */
 		GP_WIDGET_DATE		/**< \brief Date entering widget. */		/* int			*/
 	} CameraWidgetType;
-	
-	typedef enum 
-	{
-		GP_EVENT_UNKNOWN,	/**< unknown and unhandled event */
-		GP_EVENT_TIMEOUT,	/**< timeout, no arguments */
-		GP_EVENT_FILE_ADDED,	/**< CameraFilePath* = file path on camfs */
-		GP_EVENT_FOLDER_ADDED,	/**< CameraFilePath* = folder on camfs */
-		GP_EVENT_CAPTURE_COMPLETE	/**< last capture is complete */
-	} CameraEventType;
 
 	typedef struct
 	{
@@ -160,8 +149,66 @@ ffi.cdef[[
 	int gp_camera_get_config(Camera* camera, CameraWidget** window, GPContext* context);
 	int gp_camera_set_config(Camera* camera, CameraWidget* window, GPContext* context);
 	int gp_camera_get_summary(Camera* camera, CameraText* text, GPContext* context);
-	
-	int gp_camera_wait_for_event(Camera* camera, int timeout, CameraEventType* eventtype, void** eventdata, GPContext* context);
+
+
+
+	typedef int (* CameraWidgetCallback) (Camera *, CameraWidget *, GPContext *);
+
+int 	gp_widget_new 	(CameraWidgetType type, const char *label, 
+		         CameraWidget **widget);
+int    	gp_widget_free 	(CameraWidget *widget);
+int     gp_widget_ref   (CameraWidget *widget);
+int     gp_widget_unref (CameraWidget *widget);
+
+int	gp_widget_append	(CameraWidget *widget, CameraWidget *child);
+int 	gp_widget_prepend	(CameraWidget *widget, CameraWidget *child);
+
+int 	gp_widget_count_children     (CameraWidget *widget);
+int	gp_widget_get_child	     (CameraWidget *widget, int child_number, 
+				      CameraWidget **child);
+
+/* Retrieve Widgets */
+int	gp_widget_get_child_by_label (CameraWidget *widget,
+				      const char *label,
+				      CameraWidget **child);
+int	gp_widget_get_child_by_id    (CameraWidget *widget, int id, 
+				      CameraWidget **child);
+int	gp_widget_get_child_by_name  (CameraWidget *widget,
+                                      const char *name,
+				      CameraWidget **child);
+int	gp_widget_get_root           (CameraWidget *widget,
+                                      CameraWidget **root);
+int     gp_widget_get_parent         (CameraWidget *widget,
+				      CameraWidget **parent);
+
+int	gp_widget_set_value     (CameraWidget *widget, const void *value);
+int	gp_widget_get_value     (CameraWidget *widget, void *value);
+
+int     gp_widget_set_name      (CameraWidget *widget, const char  *name);
+int     gp_widget_get_name      (CameraWidget *widget, const char **name);
+
+int	gp_widget_set_info      (CameraWidget *widget, const char  *info);
+int	gp_widget_get_info      (CameraWidget *widget, const char **info);
+
+int	gp_widget_get_id	(CameraWidget *widget, int *id);
+int	gp_widget_get_type	(CameraWidget *widget, CameraWidgetType *type);
+int	gp_widget_get_label	(CameraWidget *widget, const char **label);
+
+int	gp_widget_set_range	(CameraWidget *range, 
+				 float  low, float  high, float  increment);
+int	gp_widget_get_range	(CameraWidget *range, 
+				 float *min, float *max, float *increment);
+
+int	gp_widget_add_choice     (CameraWidget *widget, const char *choice);
+int	gp_widget_count_choices  (CameraWidget *widget);
+int	gp_widget_get_choice     (CameraWidget *widget, int choice_number, 
+                                  const char **choice);
+
+int	gp_widget_changed        (CameraWidget *widget);
+int     gp_widget_set_changed    (CameraWidget *widget, int changed);
+
+int     gp_widget_set_readonly   (CameraWidget *widget, int readonly);
+int     gp_widget_get_readonly   (CameraWidget *widget, int *readonly);
 ]]
 
 local libgphoto2 = ffi.load("gphoto2")
@@ -203,71 +250,82 @@ CameraEvent.FileAdded = libgphoto2.GP_EVENT_FILE_ADDED
 CameraEvent.FolderAdded = libgphoto2.GP_EVENT_FOLDER_ADDED
 CameraEvent.CaptureComplete = libgphoto2.GP_EVENT_CAPTURE_COMPLETE
 
-local function result_to_string(result)
+local function resultToString(result)
 	return ffi.string(libgphoto2.gp_port_result_as_string(result))
 end
 
-local function acquire_camera()
+local function acquireCamera()
 	if context == nil then context = libgphoto2.gp_context_new() end
 	
-	local cameraptr = ffi.new("Camera*[1]")
-	local status = libgphoto2.gp_camera_new(cameraptr)
-	if status ~= Results.Ok then error("Failed to acquire camera: " .. result_to_string(status)) end
+	local camera = ffi.new("Camera*[1]")
+	local status = libgphoto2.gp_camera_new(camera)
+	if status ~= Results.Ok then error("Failed to acquire camera: " .. resultToString(status)) end
 
-	status = libgphoto2.gp_camera_init(cameraptr[0], context)
-	if status ~= Results.Ok then error("Failed to acquire camera: " .. result_to_string(status)) end
+	status = libgphoto2.gp_camera_init(camera[0], context)
+	if status ~= Results.Ok then error("Failed to acquire camera: " .. resultToString(status)) end
 
-	return cameraptr[0]
+	return camera[0]
 end
 
-local function lookup_widget(widget, key) 
+local function lookupWidget(widget, key) 
 	local widgetChild = ffi.new("CameraWidget*[1]")
 	local status = libgphoto2.gp_widget_get_child_by_name(widget, key, widgetChild);
 	if status ~= Results.Ok then 
 		status = libgphoto2.gp_widget_get_child_by_label(widget, key, widgetChild);
-		if status ~= Results.Ok then error("Failed to find child widget: " .. result_to_string(status)) end
+		if status ~= Results.Ok then error("Failed to find child widget: " .. resultToString(status)) end
 	end
 
 	return widgetChild[0];
 end
 
-
 local camera_methods = {}
 local camera_mt = { __index = camera_methods }
 
 function camera_methods:abilities()
-	
+	local status
+	--int gp_camera_get_abilities(Camera* camera, CameraAbilities* abilities);	
 end
-
+		
 function camera_methods:summary()
 	local text = ffi.new("CameraText")
 	local status = libgphoto2.gp_camera_get_summary(self, text, context);
-	if status ~= Results.Ok then error("Failed to get camera summary: " .. result_to_string(status)) end
+	if status ~= Results.Ok then error("Failed to get camera summary: " .. resultToString(status)) end
 
 	return ffi.string(text.text)
 end
 
-function camera_methods:get_config()
+function camera_methods:getConfigWidget()
 	local widget = ffi.new("CameraWidget*[1]")
 	local status = libgphoto2.gp_camera_get_config(self, widget, context);
-	if status ~= Results.Ok then error("Failed to get camera configuration: " .. result_to_string(status)) end
+	if status ~= Results.Ok then error("Failed to get camera configuration: " .. resultToString(status)) end
 
-	return widget[0]	
+	return widget[0]
 end
 
-function camera_methods:capture(capture_type)
-	local camera_file_path = ffi.new("CameraFilePath")
-	local status = libgphoto2.gp_camera_capture(self, capture_type, camera_file_path, context)
-	if status ~= Results.Ok then error("Failed to capture image: " .. result_to_string(status)) end
+function camera_methods:getConfig(name)
+	local widget = self:getConfigWidget()
+	local widgetChild = lookupWidget(widget, name)
+	local widgetType = libgphoto2.gp_widget_get_type(widgetChild)
 
-	return ffi.string(camera_file_path.name), ffi.string(camera_file_path.folder)
+	local target = ffi.new("void*[1]")
+
+	local status = libgphoto2.gp_widget_get_value(widgetChild, target)
+	if status ~= Results.Ok then error("Failed to get widget value: " .. resultToString(status)) end
+
+	if widgetType == libgphoto2.GP_WIDGET_TEXT then
+		return ffi.string(target[0])
+	end
 end
 
-function camera_methods:capture_image()
-	return self:capture(CameraCaptureType.Image)
+function camera_methods:lensName()
+	return getConfig("lensname")
 end
 
-function camera_methods:wait_for_event()
+function camera_methods:setConfig(name, value)
+
+end
+
+function camera_methods:waitForEvent()
 	if timeout == nil then timeout = 1000 end
 		
 	local event_type = ffi.new("CameraEventType[1]")
@@ -283,18 +341,28 @@ function camera_methods:wait_for_event()
 	return event_type[0], data
 end
 
-function camera_methods:close()
-	libgphoto2.gp_camera_exit(camera, context)
+function camera_methods:capture(capture_type)
+	local cameraFilePath = ffi.new("CameraFilePath");
+	local status = libgphoto2.gp_camera_capture(self, capture_type, cameraFilePath, context)
+	if status ~= Results.Ok then error("Failed to capture image: " .. resultToString(status)) end
+
+	return ffi.string(cameraFilePath.name), ffi.string(cameraFilePath.folder)
 end
 
+function camera_methods:captureImage()
+	return self:capture(CameraCaptureType.Image)
+end
+
+function camera_methods:close()
+	libgphoto2.gp_camera_exit(self, context)
+end
 camera_mt.__gc = camera_methods.close
 
-ffi.metatype("Camera", camera_mt)
+ffi.metatype("Camera", mt)
 
 return
 {
-	Results = Results,
+	acquireCamera = acquireCamera,
 	CameraCaptureType = CameraCaptureType,
-	CameraEvent = CameraEvent,
-	acquire_camera = acquire_camera	
+	lookupWidget = lookupWidget
 }
